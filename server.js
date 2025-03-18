a9fa828cb08d12a64552a77b3579d1eef78f5556
FranciscoGabriel <franciscogabriel.ma40788@cesurformacion.com>
2025-03-18 13:22:41 +0100

    Cambios

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const userRoutes = require('./routes/users');
+const userRoutes = require('./routes/tasks');
 
 // Configurar EJS como motor de plantillas
 app.set('view engine', 'ejs');
